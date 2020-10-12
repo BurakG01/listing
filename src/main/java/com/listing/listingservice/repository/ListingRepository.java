8c7699a13b4811c10a3aab4c403f6fb6389e0973
burak <burak.gundogdu0131@gmail.com>
2020-10-12 16:48:28 +0300

    bg adds retrieve listings query

--- src/main/java/com/listing/listingservice/repository/ListingRepository.java
+++ src/main/java/com/listing/listingservice/repository/ListingRepository.java
@@ -4,12 +4,14 @@ import com.couchbase.client.core.error.DocumentNotFoundException;
 import com.couchbase.client.java.Cluster;
 import com.couchbase.client.java.Collection;
 import com.couchbase.client.java.kv.GetResult;
+import com.couchbase.client.java.query.QueryResult;
 import com.listing.listingservice.domain.Listing;
 import com.listing.listingservice.exception.ListingNotFoundException;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 @Repository
 public class ListingRepository {
@@ -29,6 +31,7 @@ public class ListingRepository {
     public void update(Listing listing) {
         listingCollection.replace(listing.getId(), listing);
     }
+
     public Listing findById(String id) {
         try {
 
@@ -37,13 +40,15 @@ public class ListingRepository {
             return Optional.of(listing).get();
 
         } catch (DocumentNotFoundException exception) {
-            throw  new ListingNotFoundException(id);
+            throw new ListingNotFoundException(id);
         }
     }
 
-
-
     public List<Listing> getByIdList(List<String> ids) {
-        throw new ListingNotFoundException("bla");
+
+        List<String> idList = ids.stream().map(id -> "'" + id + "'").collect(Collectors.toList());
+        String statement = " SELECT sku,id,merchantId,price,stock FROM `listing` WHERE id IN " + idList;
+        QueryResult query = couchbaseCluster.query(statement);
+        return query.rowsAs(Listing.class);
     }
 }
